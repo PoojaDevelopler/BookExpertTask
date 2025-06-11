ff7b396b1cade93a352381e1d34a2a61b92d844e
pooja <pnegi@janacare.com>
2025-06-12 01:10:02 +0530

    BookExpertTaskApp class updated

--- BookExpertTask/BookExpertTaskApp.swift
+++ BookExpertTask/BookExpertTaskApp.swift
@@ -3,11 +3,7 @@ import FirebaseCore
 
 @main
 struct BookExpertTaskApp: App {
-    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
-    
-    init(){
-        requestNotificationPermission()
-    }
+
     var body: some Scene {
         WindowGroup {
             MainTabView()
